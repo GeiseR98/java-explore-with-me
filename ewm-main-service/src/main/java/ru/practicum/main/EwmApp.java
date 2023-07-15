3f4e5df081d556baddac656fbde5277757c5864d
GeiseR98 <ipatovsergejj@yandex.ru>
2023-07-15 23:15:39 +0300

    feat: добавил докеры, но контейнеры не запускаются

--- ewm-main-service/src/main/java/ru/practicum/main/EwmApp.java
+++ ewm-main-service/src/main/java/ru/practicum/main/EwmApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwmMainApp {
+public class EwmApp {
     public static void main(String[] args) {
-        SpringApplication.run(EwmMainApp.class, args);
+        SpringApplication.run(EwmApp.class, args);
     }
 }
