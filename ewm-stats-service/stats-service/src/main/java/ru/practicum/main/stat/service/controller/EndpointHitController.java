fa1557fc36a87988eda1da9da1e2a2bb6bb2833b
GeiseR98 <ipatovsergejj@yandex.ru>
2023-07-17 01:42:42 +0300

    feat: контроллер stats 1/2

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/main/stat/service/controller/EndpointHitController.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/main/stat/service/controller/EndpointHitController.java
@@ -2,13 +2,16 @@ package ru.practicum.main.stat.service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.main.stat.dto.EndpointHitDto;
+import ru.practicum.main.stat.dto.ViewStats;
 import ru.practicum.main.stat.service.service.EndpointHitService;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +31,14 @@ public class EndpointHitController {
         log.debug("Создание запроса {}" , endpointHitDto);
         endpointHitDto.setTimestamp(LocalDateTime.now());
         return service.addHit(endpointHitDto);
+    }
+
+    @GetMapping(path = "/stats")
+    public List<ViewStats> stats(@RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                 @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                 @RequestParam(name = "uris", required = false) List<String> uris,
+                                 @RequestParam(name = "unique", defaultValue = "false") boolean unique) {
+        log.debug("Попытка получить статистику по запросу {}", uris);
+        return service.stats(start, end, uris, unique);
     }
 }
--- ewm-stats-service/stats-service/src/main/java/ru/practicum/main/stat/service/repository/EndpointHitServiceImpl.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/main/stat/service/repository/EndpointHitServiceImpl.java
@@ -5,8 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.main.stat.dto.EndpointHitDto;
+import ru.practicum.main.stat.dto.ViewStats;
 import ru.practicum.main.stat.service.mapper.EndpointHitMapper;
 import ru.practicum.main.stat.service.service.EndpointHitService;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -21,5 +25,10 @@ public class EndpointHitServiceImpl implements EndpointHitService {
     public EndpointHitDto addHit(EndpointHitDto hitDto) {
         log.debug("Записи статистики успешно добавлены.");
         return mapper.toDto(repository.save(mapper.toEntity(hitDto)));
+    }
+
+    @Override
+    public List<ViewStats> stats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        return null;
     }
 }
