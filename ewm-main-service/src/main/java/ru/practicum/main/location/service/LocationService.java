2d5ea5626882009a9ad4c958451c0b7bed5d4fd8
GeiseR98 <ipatovsergejj@yandex.ru>
2023-07-26 18:55:18 +0300

    feat: location

---
+++ ewm-main-service/src/main/java/ru/practicum/main/location/service/LocationService.java
@@ -0,0 +1,8 @@
+package ru.practicum.main.location.service;
+
+import ru.practicum.main.location.dto.LocationDto;
+
+public interface LocationService {
+
+    LocationDto save(LocationDto location);
+}
