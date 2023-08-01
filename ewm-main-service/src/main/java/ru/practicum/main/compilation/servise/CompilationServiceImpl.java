802c6d235aafe49041de5468d6e4e4567ba79426
GeiseR98 <ipatovsergejj@yandex.ru>
2023-08-01 05:42:44 +0300

    feat: create all CompilationPublicController

--- ewm-main-service/src/main/java/ru/practicum/main/compilation/servise/CompilationServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main/compilation/servise/CompilationServiceImpl.java
@@ -2,6 +2,7 @@ package ru.practicum.main.compilation.servise;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.main.compilation.dto.CompilationDto;
@@ -12,9 +13,11 @@ import ru.practicum.main.compilation.model.Compilation;
 import ru.practicum.main.compilation.repository.CompilationRepository;
 import ru.practicum.main.event.model.Event;
 import ru.practicum.main.exception.NotFoundException;
+import ru.practicum.main.utility.Page;
 import ru.practicum.main.utility.Utility;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -56,4 +59,21 @@ public class CompilationServiceImpl implements CompilationService {
         log.debug("Подборка обновлена");
         return mapper.toDto(compilationRepository.save(oldCompilations));
     }
+
+    @Override
+    public CompilationDto getCompilationsById(Integer compId) {
+        log.debug("Подборка событий найдена");
+        return mapper.toDto(utility.checkCompilation(compId));
+    }
+
+    @Override
+    public List<CompilationDto> getCompilations(Boolean pinned, Integer from, Integer size) {
+        Pageable page = Page.paged(from, size);
+        log.debug("Подборка событий найдена");
+        return compilationRepository.findCompilationsByPinnedIs(pinned, page).stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+
 }
